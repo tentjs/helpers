966410fd0ffc61a6a407477b51b30d4f94280a6b
sebkolind <sks1993@gmail.com>
2024-06-02 21:23:03 +0200

    fix(ternary): export in main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bind } from "./bind";
 import { on, type OnEvents } from "./on";
 import { classes } from "./classes";
+import { ternary } from "./ternary";
 
 import type { FormEvent } from "./types";
 
-export { bind, on, classes, type OnEvents, type FormEvent };
+export { bind, on, classes, ternary, type OnEvents, type FormEvent };
